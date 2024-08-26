b7362f8eb65bef2ebf252cc32d07ed37ae64f9cc
Ilkka Kokkarinen <ilkka.kokkarinen@gmail.com>
2024-08-25 20:00:21 -0400

    Add files via upload
    Cleaned up the code and added second argument for the word to end the search at.

--- wordsquare.c
+++ wordsquare.c
@@ -1,6 +1,6 @@
 /*
 Author: Ilkka Kokkarinen, ilkka.kokkarinen@gmail.com
-Date: Aug 2, 2024, update 2
+Date: Aug 25, 2024
 GitHub: https://github.com/ikokkari/Wordsquare
 */
 
@@ -9,7 +9,10 @@ GitHub: https://github.com/ikokkari/Wordsquare
 #include "string.h"
 #include "assert.h"
 
-/* If we are verbose about the first word. */
+/* Big number to mean "none" */
+#define M 1000000
+
+/* Output progress report about what is happening. */
 #define VERBOSE 1
 
 /* Size of grid square */
@@ -30,6 +33,12 @@ typedef unsigned long ulong;
 /* Two-letter prefixes that exist in words. */
 uint prefixes[26][26];
 
+/* Starting positions of each one character prefix in the wordlist. */
+uint one_start[26];
+
+/* Starting position of each two character prefix in the wordlist. */
+uint two_start[26][26];
+
 /* For each cell of the square, the characters that are still possible for that cell,
    encoded as a bit vector in the lowest 26 bits of the unsigned integer. */
 uint remain[N][N];
@@ -43,8 +52,8 @@ char word_buffer[2 * N][N + 1];
 
 /* The undo stack used to store the operations to restore previous state in backtracking. */
 uint* undo;
-int undo_top = 0;
-int undo_capacity = 100;
+uint undo_top = 0;
+uint undo_capacity = 100;
 
 /* Rows and columns that need to be checked in consistency propagation. */
 uint* to_check;
@@ -53,13 +62,16 @@ uint* to_check;
 uint* taken;
 
 /* The possible words used to fill in the double word square. */
-int word_count = 0;
+uint word_count = 0;
 char wordlist[MAXWORDS][N + 1];
 
 /* The start of the words to try in the first row. */
-char* first = "";
-uint first_len = 0;
+char* first = NULL;
 uint first_idx = 0;
+
+/* The end of the words to try in the first row. */
+char* last = NULL;
+uint last_idx = 0;
 
 /* Index to wordlist of the word on the first row. */
 uint first_row_idx = 0;
@@ -67,43 +79,44 @@ uint first_row_idx = 0;
 /* Bookkeeping statistics for effectiveness of remain pruning. */
 ulong remain_cutoffs = 0;
 ulong prefix_cutoffs = 0;
-int max_level = 0;
+uint max_level = 0;
 
 /* Read the list of words from the file words_sorted.txt, keeping the words of length N. */
 void read_wordlist() {
+  for(uint i = 0; i < 26; i++) {
+    one_start[i] = M;
+    for(uint j = 0; j < 26; j++) {
+      two_start[i][j] = M;
+    }
+  }
   char buffer[100];
   FILE* file = fopen("words_sorted.txt", "r");
   while(fgets(buffer, sizeof buffer, file) != NULL) {
-    int j = 0;
+    uint j = 0;
     while('a' <= buffer[j] && buffer[j] <= 'z') {
       j++;
     }
     buffer[j] = '\0';
     if(j == N) {
-      strcpy(wordlist[word_count++], buffer);
+      strcpy(wordlist[word_count], buffer);
+      if(one_start[buffer[0] - 'a'] > word_count) {
+	one_start[buffer[0] - 'a'] = word_count;
+      }
+      if(two_start[buffer[0] - 'a'][buffer[1] - 'a'] > word_count) {
+	two_start[buffer[0] - 'a'][buffer[1] - 'a'] = word_count;
+      }
+      word_count++;
     }
   }
   fclose(file);
-  if(VERBOSE) {
-    printf("Read a total of %d words of length %d.\n\n", word_count, N);
-  }
   taken = calloc(word_count, sizeof(uint));
-
-  /* Fill up the table of two-letter prefixes that exist in the wordlist. */
-  for(int i = 0; i < 26; i++) {
-    for(int j = 0; j < 26; j++) {
-      prefixes[i][j] = 0;
-    }
-  }
-  for(int i = 0; i < word_count; i++) {
-    prefixes[wordlist[i][0]-'a'][wordlist[i][1]-'a'] = 1;
-  }
+  last_idx = word_count;
 }
 
 /* Print the contents of the current square. */
 void print_square() {
-  for(int i = 0; i < N; i++) {
-    for(int j = 0; j < N; j++) {
+  for(uint i = 0; i < N; i++) {
+    for(uint j = 0; j < N; j++) {
       printf("%c", square[i][j]);
     }
     printf("\n");
@@ -112,7 +125,7 @@ void print_square() {
 }
 
 /* Push an item into the undo stack, expanding the stack space if needed. */
-void undo_push(int item) {
+void undo_push(uint item) {
   if(undo_top == undo_capacity) {
     undo_capacity *= 2;
     undo = realloc(undo, undo_capacity * sizeof(uint));
@@ -121,8 +134,8 @@ void undo_push(int item) {
 }
 
 /* Place the word into the square starting at cell (x, y) into direction (dx, dy). */
-void place_word(char* word, int x, int y, int dx, int dy) {
-  for(int j = 0; j < N; j++) { 
+void place_word(char* word, uint x, uint y, uint dx, uint dy) {
+  for(uint j = 0; j < N; j++) { 
     if(square[x][y] == '.') {
       square[x][y] = word[j];
       undo_push(x);
@@ -144,13 +157,13 @@ void place_word(char* word, int x, int y, int dx, int dy) {
 
 /* Determine if the given word would fit into the square starting at cell (x, y)
    into direction (dx, dy). */
-int word_fits(char* word, int x, int y, int dx, int dy) {
-  for(int i = 0; i < N; i++) {
+uint word_fits(char* word, uint x, uint y, uint dx, uint dy) {
+  for(uint i = 0; i < N; i++) {
     char ch = word[i];
     if(square[x][y] != '.' && square[x][y] != ch) {
       return 0;
     }
-    if(square[x][y] == '.' && (remain[x][y] & (1 << (ch-'a'))) == 0) {
+    if(square[x][y] == '.' && (remain[x][y] & (1 << (ch - 'a'))) == 0) {
       return 0;
     }
     x += dx;
@@ -161,8 +174,8 @@ int word_fits(char* word, int x, int y, int dx, int dy) {
 
 /* Find the longest contiguous prefix in the square starting from position (x, y)
    into direction (dx, dy). */
-void find_prefix(char* buffer, int x, int y, int dx, int dy) {
-  int i = 0;
+void find_prefix(char* buffer, uint x, uint y, uint dx, uint dy) {
+  uint i = 0;
   while(i < N) {
     if(square[x][y] != '.') {
       buffer[i++] = square[x][y];
@@ -179,14 +192,24 @@ void find_prefix(char* buffer, int x, int y, int dx, int dy) {
 
 /* Binary search algorithm to find the position of the first word that starts with 
    the given prefix. */ 
-int bisect_left(char* prefix) {
+uint bisect_left(char* prefix) {
+  /* Look up the precomputed results for one- and two-character prefixes. */
+  if(prefix[1] == '\0') {
+    return one_start[prefix[0] - 'a'];
+  }
+  if(prefix[2] == '\0') {
+    return two_start[prefix[0] - 'a'][prefix[1] - 'a'];
+  }
+  
   /* Prefix is after the last word in the wordlist. */
   if(strcmp(wordlist[word_count - 1], prefix) < 0) {
     return word_count;
   }
-  int lo = 0, hi = word_count;
+
+  /* Use binary search to find the first word that starts with the given prefix. */
+  uint lo = 0, hi = word_count - 1;
   while(lo < hi) {
-    int mid = (lo + hi) / 2;
+    uint mid = (lo + hi) / 2;
     if(strcmp(wordlist[mid], prefix) < 0) {
       lo = mid + 1;
     }
@@ -198,8 +221,8 @@ int bisect_left(char* prefix) {
 }
 
 /* Determines if the first word is a prefix of the second word. */
-int starts_with(char* first, char* second) {
-  int i = 0;
+uint starts_with(char* first, char* second) {
+  uint i = 0;
   while(1) {
     if(first[i] == '\0') { return 1; }
     if(first[i] != second[i]) { return 0; }
@@ -209,18 +232,18 @@ int starts_with(char* first, char* second) {
 
 /* Update the remaining characters table for the row or column that starts at (x, y)
    and goes in direction (dx, dy). Returns 1 if dead end reached, 0 otherwise. */
-int update_one_remain(int x, int y, int dx, int dy) {
+uint update_one_remain(uint x, uint y, uint dx, uint dy) {
   uint possible[N];
   char buffer[N + 1];
-  for(int j = 0; j < N; j++) { possible[j] = 0; }
+  for(uint j = 0; j < N; j++) { possible[j] = 0; }
   find_prefix(buffer, x, y, dx, dy);
-  int i = bisect_left(buffer);
+  uint i = bisect_left(buffer);
   if(i >= word_count || !starts_with(buffer, wordlist[i])) {
     return 1;
   }
   while(i < word_count && starts_with(buffer, wordlist[i])) {
     if(word_fits(wordlist[i], x, y, dx, dy)) {
-      for(int j = 0; j < N; j++) {
+      for(uint j = 0; j < N; j++) {
 	if(square[x + dx * j][y + dy * j] == '.') {
 	  possible[j] |= 1 << (wordlist[i][j] - 'a');
 	}
@@ -228,8 +251,8 @@ int update_one_remain(int x, int y, int dx, int dy) {
     }
     i++;
   }
-  for(int j = 0; j < N; j++) {
-    int xx = x + dx * j, yy = y + dy * j;
+  for(uint j = 0; j < N; j++) {
+    uint xx = x + dx * j, yy = y + dy * j;
     if(square[xx][yy] == '.') {
       uint new_remain = possible[j] & remain[xx][yy];
       if(new_remain == 0) {
@@ -255,11 +278,11 @@ int update_one_remain(int x, int y, int dx, int dy) {
 }
 
 /* Perform consistency propagation throughout the entire square until convergence. */
-int update_all_remains(int level) {
-  int result;
+uint update_all_remains(uint level) {
+  uint result;
   while(1) {
     /* First level that needs checking. */
-    int v = level;
+    uint v = level;
     while(v < 2 * N) {
       if(to_check[v]) { break; }
       v++;
@@ -272,9 +295,9 @@ int update_all_remains(int level) {
 }
 
 /* Verify that the two words on the first two rows are possible to complete into column words. */  
-int verify_col_prefixes() {
-  for(int j = 1; j < N; j++) {
-    if(prefixes[square[0][j]-'a'][square[1][j]-'a'] == 0) {
+uint verify_col_prefixes() {
+  for(uint j = 1; j < N; j++) {
+    if(two_start[square[0][j] - 'a'][square[1][j] - 'a'] == M) {
       prefix_cutoffs++;
       return 0;
     }
@@ -283,9 +306,9 @@ int verify_col_prefixes() {
 }
 
 /* Verify that the two words on the first two columns are possible to complete into row words. */
-int verify_row_prefixes() {
-  for(int i = 2; i < N; i++) {
-    if(prefixes[square[i][0]-'a'][square[i][1]-'a'] == 0) {
+uint verify_row_prefixes() {
+  for(uint i = 2; i < N; i++) {
+    if(two_start[square[i][0]-'a'][square[i][1]-'a'] == M) {
       prefix_cutoffs++;
       return 0;
     }
@@ -295,7 +318,7 @@ int verify_row_prefixes() {
 
 /* Pop and execute instructions from the undo stack to restore previous state in backtracking. */
 void unroll_choices() {
-  int item = undo[--undo_top];
+  uint item = undo[--undo_top];
   while(item != UNDO_DONE) {
     if(item == UNDO_PLACE) {
       uint y = undo[--undo_top];
@@ -312,13 +335,13 @@ void unroll_choices() {
 }
 
 /* Recursive backtracking algorithm to fill in the double word square. */
-void fill_square(int level) {
+void fill_square(uint level) {
   if(level > max_level) { max_level = level; }
   if(level == 2 * N) { /* The grid is complete and ready to be printed out. */
     print_square();
     return;
   }
-  int x, y, dx, dy;
+  uint x, y, dx, dy;
   if(level & 1) { /* Fill in a vertical column at this level of recursion. */
     x = 0; y = level / 2; dx = 1; dy = 0;
   }
@@ -326,8 +349,8 @@ void fill_square(int level) {
     x = level / 2; y = 0; dx = 0; dy = 1;
   }
   find_prefix(word_buffer[level], x, y, dx, dy);
-  int i = level == 0? first_idx : bisect_left(word_buffer[level]);
-  while(i < word_count && (level != 1 || i < first_row_idx) && starts_with(word_buffer[level], wordlist[i])) {
+  uint i = level == 0 ? first_idx : bisect_left(word_buffer[level]);
+  while(i < (level == 0 ? last_idx: word_count) && (level != 1 || i < first_row_idx) && starts_with(word_buffer[level], wordlist[i])) {
     if(!taken[i] && word_fits(wordlist[i], x, y, dx, dy)) {
       if(level == 0) {
         first_row_idx = i;
@@ -336,7 +359,7 @@ void fill_square(int level) {
 		   i - first_idx, wordlist[i], remain_cutoffs, prefix_cutoffs, max_level);
         }
       }
-      for(int j = 0; j < 2 * N; j++) { to_check[j] = 0; }
+      for(uint j = 0; j < 2 * N; j++) { to_check[j] = 0; }
       undo_push(UNDO_DONE);
       place_word(wordlist[i], x, y, dx, dy);
       if(
@@ -356,31 +379,43 @@ void fill_square(int level) {
 
 /* Main driver of the backtracking algorithm. */
 int main(int argc, char** argv) {
-int y = 0;
+  uint y = 0;
   read_wordlist();
   undo = malloc(sizeof(uint) * undo_capacity);
   to_check = calloc(3 * N, sizeof(uint));
-  for(int x = 0; x < N; x++) {
-    for(int y = 0; y < N; y++) {
+  for(uint x = 0; x < N; x++) {
+    for(uint y = 0; y < N; y++) {
       remain[x][y] = 0x3FFFFFF; /* Integer with the lowest 26 bits on. */
       square[x][y] = '.'; /* Grid is initially all empty. */
     }
   }
+
   /* Place the prefix on the first row. */
   if(argc > 1) {
-    first_len = strlen(argv[1]);
-    first = malloc(first_len + 1);
+    first = malloc(strlen(argv[1]) + 1);
     strcpy(first, argv[1]);
     first_idx = bisect_left(first);
   }
 
+  if(argc > 2) {
+    last = malloc(strlen(argv[2]) + 1);
+    strcpy(last, argv[2]);
+    last_idx = bisect_left(last);
+  }
+
+  printf("Starting search from %d to %d.\n", first_idx, last_idx);
+  
   /* Do the watussi, Johnny */
   fill_square(0);
 
+  printf("Ending search from %d to %d.\n", first_idx, last_idx);
+  
   /* So that Valgrind won't complain about memory leaks. */
   free(undo);
   free(to_check);
   free(taken);
+  free(first);
+  free(last);
 
   /* All is well in the world. */
   return 0;
